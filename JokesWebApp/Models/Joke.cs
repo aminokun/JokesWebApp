a9bf41ca0eb61f87ee44b337f1cb89860b39849a
amine <amine.dbh99@gmail.com>
2023-03-01 13:08:33 +0100

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int iD { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnwser { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
